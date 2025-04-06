124dd22228b0cf68a7bc58d1df03c4585b25d8c2
BlueandRedandWhite <9203460@alumnos.ufv.es>
2025-04-06 13:48:37 +0200

    Ubicacion de Repartidor solo con vista desde repartidor

--- mi_proyecto/context_processors.py
+++ mi_proyecto/context_processors.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-
+from django.contrib.auth.models import Group
 def avatar_context(request):
     if request.user.is_authenticated:
         avatar_path = "img/avatares/hombre.png" if request.user.genero == "hombre" else "img/avatares/mujer.png"
@@ -9,13 +9,14 @@ def avatar_context(request):
     return {"avatar_path": avatar_path}
 
 def user_groups_processor(request):
-    """ Agrega los grupos del usuario al contexto de las plantillas. """
     if request.user.is_authenticated:
         user_groups = list(request.user.groups.values_list("name", flat=True))
+        is_repartidor = "repartidor" in user_groups
     else:
         user_groups = []
+        is_repartidor = False
 
-    return {"user_groups": user_groups}
+    return {"user_groups": user_groups, "is_repartidor": is_repartidor}
 
 
 
